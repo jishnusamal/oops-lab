39abd9c17b9ad5cbda666ec3023bab3a205d71c9
Jishnu Prasad Samal <dev@jishnupsamal.me>
2026-01-31 23:27:49 +0530

    Refactor Mixer class to improve sorting and merging functionality; update MixerDemo for better demonstration

--- Lab_3/MixerDemo.java
+++ Lab_3/MixerDemo.java
@@ -1,66 +1,97 @@
 package Lab_3;
 
 class Mixer {
-	int[] arr;
-	// int[] newArr;
+    int[] arr;
 
-	Mixer(int[] arr) {
-		this.accept(arr);
-	}
-	
-	void accept(int[] arr) {
-		int n = arr.length;
-		for (int i = 0; i<n-1; i++) {
-			int smallest = arr[i];
-			int pos = i;
-			for (int j = i+1; j<n; j++) {
-				if (arr[j] < smallest) {
-					smallest = arr[j];
-					pos = j;
-				}
-			}
-			int temp = arr[i];
-			arr[i] = smallest;
-			arr[pos] = temp;
-		}
-		
-		this.arr = arr;
-	}
-	
-	void mix (Mixer a) {
-		int temp[] = new int[this.arr.length + a.arr.length];
+    Mixer(int[] input) {
+        accept(input);
+    }
 
-		for (int i = 0; i<this.arr.length; i++) {
-			temp[i] = this.arr[i];
-		}
-		
-		for (int i = 0; i<a.arr.length; i++) {
-			temp[i+this.arr.length] = a.arr[i];
-		}
+    // Accept array in ascending order without duplicates
+    void accept(int[] input) {
 
-		this.accept(temp);
-	}
+        // Selection Sort
+        for (int i = 0; i < input.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[min]) {
+                    min = j;
+                }
+            }
+            int temp = input[i];
+            input[i] = input[min];
+            input[min] = temp;
+        }
 
-	void display () {
-		for (int e: this.arr) {
-			System.out.print(e + " ");
-		}
-		System.out.println();
-	}
+        // Remove duplicates
+        int temp[] = new int[input.length];
+        int k = 0;
+
+        temp[k++] = input[0];
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] != input[i - 1]) {
+                temp[k++] = input[i];
+            }
+        }
+
+        arr = new int[k];
+        for (int i = 0; i < k; i++) {
+            arr[i] = temp[i];
+        }
+    }
+
+    // Merge two Mixer objects
+    Mixer mix(Mixer A) {
+
+        int i = 0, j = 0, k = 0;
+        int temp[] = new int[this.arr.length + A.arr.length];
+
+        while (i < this.arr.length && j < A.arr.length) {
+            if (this.arr[i] < A.arr[j]) {
+                temp[k++] = this.arr[i++];
+            } else if (this.arr[i] > A.arr[j]) {
+                temp[k++] = A.arr[j++];
+            } else {
+                temp[k++] = this.arr[i++];
+                j++;
+            }
+        }
+
+        while (i < this.arr.length) {
+            temp[k++] = this.arr[i++];
+        }
+
+        while (j < A.arr.length) {
+            temp[k++] = A.arr[j++];
+        }
+
+        int result[] = new int[k];
+        for (int x = 0; x < k; x++) {
+            result[x] = temp[x];
+        }
+
+        return new Mixer(result);
+    }
+
+    void display() {
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
 }
 
 public class MixerDemo {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		int[] arr1 = {5, 3, 8, 1, 2};
-		int[] arr2 = {7, 4, 6, 9, 0};
-		
-		Mixer mixer1 = new Mixer(arr1);
-		Mixer mixer2 = new Mixer(arr2);
+        int[] arr1 = {5, 3, 8, 1, 2, 3, 5, 7};
+        int[] arr2 = {7, 4, 6, 9, 0};
 
-		mixer1.mix(mixer2);
-		mixer1.display();
-		// mixer2.display();
-	}
+        Mixer m1 = new Mixer(arr1);
+        Mixer m2 = new Mixer(arr2);
 
+        Mixer m3 = m1.mix(m2);
+
+        m3.display();
+    }
 }
